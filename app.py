13aae142c3c9ce7ddc1c6ab6e70a4954ffc7b310
Prakash Kumar <prakashkumar09031991@gmail.com>
2025-09-08 20:26:22 +0530

    Initial commit

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Kubernetes CI/CD pipeline! This is the first time I experimented with CICD pipeline. so basically waht happens:Push code to GitHub → GitHub Actions builds Docker image → pushes to GitHub Container Registry.
-
-ArgoCD auto-syncs repo → updates Kubernetes → deploys new version.
-
-Your app is live with CI/CD on Kubernetes 🎯"
+    return "Hello from Kubernetes CI/CD pipeline! This is the first time I experimented with CICD pipeline. so basically waht happens:"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
